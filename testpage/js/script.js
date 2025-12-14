1698c2aa8ebb5ae691ae458fd9f21f86d9b9b2bf
yoon-oyo <harukayoon.0914@gmail.com>
2025-12-14 17:27:10 +0900

    Delete testpage directory

--- testpage/js/script.js
+++
@@ -1,8 +0,0 @@
-/* ===== script.js ===== */
-const ham = document.getElementById("hamburger");
-const nav = document.getElementById("mobile-nav");
-
-
-ham.addEventListener("click", () => {
-nav.style.display = nav.style.display === "block" ? "none" : "block";
-});
